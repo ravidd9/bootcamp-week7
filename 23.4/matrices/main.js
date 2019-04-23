8073e5a4c94483fe0d0ee062876204b18dd901c5
Ravid Cohen <ravidd9@gmail.com>
2019-04-23 15:37:27 +0300

    finished exercises!

--- 23.4/matrices/main.js
+++ 23.4/matrices/main.js
@@ -1,10 +1,8 @@
 class Matrix {
     constructor(rows, cols) {
-        this.rows = rows
-        this.cols = cols
         this.matrix = []
-        this.generateMatrix(this.rows, this.cols)
-        
+        this.generateMatrix(rows, cols)
+
     }
 
     generateMatrix(numRows, numColumns) {
@@ -47,15 +45,18 @@ class Matrix {
         }
     }
 
-    findCoordinate(value){
-        let coor = {x: null, y: null}
-        for(let i = 0; i < this.matrix.length; i++){
-            for(let j=0; j< this.matrix[i].length; j++){
-                if(this.matrix[i][j] == value){
+    findCoordinate(value) {
+        let coor = {
+            x: null,
+            y: null
+        }
+        for (let i = 0; i < this.matrix.length; i++) {
+            for (let j = 0; j < this.matrix[i].length; j++) {
+                if (this.matrix[i][j] == value) {
                     coor.x = j
                     coor.y = i
                     return coor
-                } 
+                }
             }
         }
     }
@@ -82,50 +83,91 @@ class Matrix {
 // console.log(m.findCoordinate(7)) //prints {x: 2, y: 1}
 
 
-class EmployeeMatrix extends Matrix{
-    constructor(){
+class EmployeeMatrix extends Matrix {
+    constructor() {
         super()
     }
 
-    loadData(salaryData){
-        for(let salary of salaryData){
+    loadData(salaryData) {
+        for (let salary of salaryData) {
             this.matrix.push([salary._id, salary.name, salary.department, salary.salary])
         }
     }
 
-    getEmployees(department){
+    getEmployees(department) {
         let depArr = []
-        for(let i = 0; i< this.matrix.length; i++){
-            if(this.matrix[i][2] == department){
+        for (let i = 0; i < this.matrix.length; i++) {
+            if (this.matrix[i][2] == department) {
                 depArr.push(this.matrix[i][1])
             }
         }
         return depArr
     }
 
-    getTotalSalary(department){
+    getTotalSalary(department) {
         let depSal = 0
-        for(let i = 0; i< this.matrix.length; i++){
-            if(this.matrix[i][2] == department){
+        for (let i = 0; i < this.matrix.length; i++) {
+            if (this.matrix[i][2] == department) {
                 depSal += this.matrix[i][3]
             }
         }
         return depSal
     }
+
+    findRichest() {
+        let maxSal = 0
+        let richest = ""
+        for (let i = 0; i < this.matrix.length; i++) {
+            if (this.matrix[i][3] > maxSal) {
+                maxSal = this.matrix[i][3]
+                richest = this.matrix[i][1]
+            }
+        }
+        return richest
+    }
 }
 //3
-let data = [
-    { _id: "e10021", name: "Gillian", department: "Finance", salary: 2000 },
-    { _id: "e10725", name: "Tibor", department: "Design", salary: 1200 },
-    { _id: "e10041", name: "Anisha", department: "Finance", salary: 2300 },
-    { _id: "e10411", name: "Jakub", department: "Design", salary: 1600 },
-    { _id: "e11995", name: "Mar", department: "Design", salary: 1300 },
-    { _id: "e10732", name: "Nisha", department: "Design", salary: 1200 }
+let data = [{
+        _id: "e10021",
+        name: "Gillian",
+        department: "Finance",
+        salary: 2000
+    },
+    {
+        _id: "e10725",
+        name: "Tibor",
+        department: "Design",
+        salary: 1200
+    },
+    {
+        _id: "e10041",
+        name: "Anisha",
+        department: "Finance",
+        salary: 2300
+    },
+    {
+        _id: "e10411",
+        name: "Jakub",
+        department: "Design",
+        salary: 1600
+    },
+    {
+        _id: "e11995",
+        name: "Mar",
+        department: "Design",
+        salary: 1300
+    },
+    {
+        _id: "e10732",
+        name: "Nisha",
+        department: "Design",
+        salary: 1200
+    }
 ]
 
-let m = new EmployeeMatrix()
-m.loadData(data)
-m.print()
+// let m = new EmployeeMatrix()
+// m.loadData(data)
+// m.print()
 
 //4
 // console.log(m.getEmployees("Finance")) //prints [ 'Gillian', 'Anisha' ]
@@ -136,11 +178,64 @@ m.print()
 // console.log(m.getTotalSalary("Design")) //prints 5300
 
 //6
+// console.log(m.findRichest()) //prints Anisha
+
+//7 + 8
+class TicTacToe extends Matrix {
+    constructor() {
+        super()
+    }
+
+    loadBoard() {
+        for (let r = 0; r < 3; r++) {
+            this.matrix.push([])
+            for (let c = 0; c < 3; c++) {
+                this.matrix[r].push(".")
+            }
+        }
+    }
+
+    checkColumn(colNum) {
+        let col = ""
+        for (let i = 0; i < this.matrix.length; i++) {
+            col += this.matrix[i][colNum]
+        }
+        return col
+    }
+
+    checkRow(rowNum) {
+        let row = ""
+        for (let i = 0; i < this.matrix[rowNum].length; i++) {
+            row += this.matrix[rowNum][i]
+        }
+        return row
+    }
+
+    play(r, c, p) {
+        p == 1 ? this.alter(r, c, "X") : this.alter(r, c, "O")
+        let row = this.checkRow(r)
+        let col = this.checkColumn(c)
+        let p1w = "XXX"
+        let p0w = "OOO"
+        if ((row == p1w) || (row == p0w) ||
+        (col == p1w) || (col == p0w))
+        {
+            console.log(`Congratulations Player ${p}`)
+        }
+    }
+}
+
+let board = new TicTacToe()
+board.loadBoard()
+// board.play(2, 2, 1)
+// board.play(0, 0, 2)
+// board.print()
 
 
-
-
-
-
-
-
+board.play(2, 2, 1)
+board.play(0, 0, 0)
+board.play(0, 2, 1)
+board.play(1, 0, 0)
+board.play(1, 2, 1) //prints Congratulations Player 1
+board.print()
+//prints
